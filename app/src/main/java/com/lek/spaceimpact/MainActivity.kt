56a27ef6a6e59cfa58a4d53d04756e030ca3045a
Aliyu <aliyu.olalekan13@gmail.com>
2023-10-14 17:50:08 +0200

    Added game components and render logic

--- app/src/main/java/com/lek/spaceimpact/MainActivity.kt
+++ app/src/main/java/com/lek/spaceimpact/MainActivity.kt
@@ -1,34 +1,36 @@
 package com.lek.spaceimpact
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
 import com.lek.spaceimpact.component.GameView
-import com.lek.spaceimpact.component.dpToPx
-import com.lek.spaceimpact.component.pxToDp
+import com.lek.spaceimpact.ui.state.DownDirectionClicked
+import com.lek.spaceimpact.ui.state.GamePaused
+import com.lek.spaceimpact.ui.state.GunFired
+import com.lek.spaceimpact.ui.state.KeyReleased
+import com.lek.spaceimpact.ui.state.LeftDirectionClicked
+import com.lek.spaceimpact.ui.state.RightDirectionClicked
+import com.lek.spaceimpact.ui.state.UpDirectionClicked
 import com.lek.spaceimpact.ui.theme.SpaceImpactTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    val viewModel: GameViewModel by viewModels()
+    private val viewModel: GameViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +38,6 @@ class MainActivity : ComponentActivity() {
             SpaceImpactTheme {
                 val state = viewModel.state.collectAsState().value
                 // A surface container using the 'background' color from the theme
-
 
                 Surface(
                     modifier = Modifier
@@ -48,9 +49,6 @@ class MainActivity : ComponentActivity() {
                     }
                     var gameStarted by remember {
                         mutableStateOf(false)
-                    }
-                    val heightChanged by remember {
-                        derivedStateOf { size != IntSize(0, 0) }
                     }
                     Box(
                         modifier = Modifier
@@ -71,7 +69,25 @@ class MainActivity : ComponentActivity() {
                                     )
                                     gameStarted = true
                                 }
-                            }
+                            },
+                            onLeftPressed = {
+                                Log.d("PRESSED", "LEFT")
+                                viewModel.onEvent(LeftDirectionClicked)
+                            },
+                            onRightPressed = {
+                                viewModel.onEvent(RightDirectionClicked)
+                            },
+                            onUpPressed = {
+                                viewModel.onEvent(UpDirectionClicked)
+                            },
+                            onDownPressed = {
+                                viewModel.onEvent(DownDirectionClicked)
+                            },
+                            onFireBulletPressed = {
+                                viewModel.onEvent(GunFired)
+                            },
+                            onKeyReleased = { viewModel.onEvent(KeyReleased) },
+                            onPause = { viewModel.onEvent(GamePaused) }
                         )
                     }
                 }
--- app/src/main/java/com/lek/spaceimpact/ui/EnemiesGenerator.kt
+++ app/src/main/java/com/lek/spaceimpact/ui/EnemiesGenerator.kt
@@ -7,7 +7,7 @@ object EnemiesGenerator {
     fun generateEnemies(screenWidth: Int, count: Int = 10, enemySize: Int): List<Enemy> {
         val startOffset = (-enemySize * count until -enemySize).step(enemySize * 3).toList()
 
-        val spaced = (Enemy.ENEMY_SIZE.toInt()..(screenWidth - (Enemy.ENEMY_SIZE.toInt() / 2))).step(enemySize * 3).toList()
+        val spaced = (Enemy.ENEMY_SIZE.toInt()..(screenWidth - (Enemy.ENEMY_SIZE.toInt()))).step(enemySize * 3).toList()
 
         return (0 until count).map {
             Enemy(
